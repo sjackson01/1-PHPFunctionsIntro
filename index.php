f469859755bdf8861dfe33077e221e51e3d514a9
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-03 15:38:14 -0600

    Add: Conditional function global var

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+/* !Function Intro! */
+
+/*
+//Simple function
+function hello(){
+    echo 'Hello, World';
+}
+//Call function
+hello();
+*/
+
+//Condtional Function
+$current_user = 'Mike';
+function is_mike(){
+    //Access variable without argument
+    global $current_user;
+    if($current_user == 'Mike'){
+        echo 'It is Mike.';
+    } else {
+        echo 'It is not Mike.';
+    }
+}  
+
+//Call function
+is_mike();
 
 ?>
