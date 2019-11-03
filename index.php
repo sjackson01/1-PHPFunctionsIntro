0907816f8a393fb15407e788223f8f5146fe654e
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-03 16:17:13 -0600

    Add: 1 argument array function

--- index.php
+++ index.php
@@ -31,5 +31,35 @@ function is_mike(){
 //Call function
 is_mike();
 */
+/*
+//Create function with one argument
+function hello($name){
+    //Expand and echo $name variable 
+    echo "Hello, $name, how's it going?";
+
+}
+//Call function and pass argument
+hello('Hampton');
+*/
+
+//Pass in multiple arguments with an array 
+function hello($arr){
+    if(is_array($arr)){
+        foreach($arr as $name)
+        {
+        echo "Hell, $name, how's it goin! <br />";
+        }
+    }else{
+        echo 'Hello, friends';
+    }
+}
+//Creat Array
+$names = array(
+    'Hampton', 
+    'Charlie',
+    'Mike',
+);
+
+hello($names);
 
 ?>
